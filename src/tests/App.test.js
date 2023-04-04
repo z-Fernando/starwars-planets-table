d7ea17a935f644a59ce8aab1745feb410ff53e11
Fernando F <prog.lffp@gmail.com>
2023-04-04 10:32:59 -0300

    Requisito 05: Testando Loading e Inputs na tela

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,37 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from '../App';
+import mockFetch from './helpers/mockFetch';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Testando a tela inicial da aplicação pós Loading.', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn(mockFetch);
+  });
+
+  test('Testa se o <Loading /> está funcional', async () => {
+    render(<App />);
+    const loadingComponent = screen.getByText('Loading...');
+    expect(loadingComponent).toBeInTheDocument();
+    expect(loadingComponent).toBeDefined()
+    await waitFor(() => {
+      const inputName = screen.getByTestId('name-filter');
+      expect(inputName).toBeInTheDocument();
+      expect(inputName).toBeDefined()
+    })
+  });
+
+  test('Testa se os Inputs estão na tela', async () => {
+    render(<App />);
+    const loadingComponent = screen.getByText('Loading...');
+    expect(loadingComponent).toBeInTheDocument();
+    expect(loadingComponent).toBeDefined()
+    await waitFor(() => {
+      const inputName = screen.getByTestId('name-filter');
+      expect(inputName).toBeInTheDocument();
+      expect(inputName).toBeDefined()
+    })
+    const columnFilter = screen.getByTestId('column-filter');
+    expect(columnFilter).toBeInTheDocument();
+    expect(columnFilter).toBeDefined()
+  });
 });
